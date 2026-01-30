4c79ce69624ad1d335c48c728199588902b1eeff
Tobey Codes <tobey@tobey.codes>
2026-01-30 13:15:16 +0700

    feat: add orbstack to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,7 @@ starship init fish | source
 fzf --fish | source
 
 zoxide init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
