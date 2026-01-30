5c14ae9d3e599672ebfd588de71c72e76dfc4ee0
Tobey Codes <tobey@tobey.codes>
2026-01-30 10:54:36 +0700

    feat: add android home for react native

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+set --export ANDROID_HOME $HOME/Library/Android/sdk
+set -gx PATH $ANDROID_HOME/emulator $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+
 bind \cf '~/.local/bin/tmux-sessions'
 
 eval "$(/opt/homebrew/bin/brew shellenv fish)"
