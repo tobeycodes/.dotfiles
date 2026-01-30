66f97ddcb849b5bb288daf159b08fc630e34f3ce
Tobey Codes <tobey@tobey.codes>
2026-01-30 12:03:23 +0700

    wip

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'biomejs' },
+        javascriptreact = { 'biomejs' },
+        typescript = { 'biomejs' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
