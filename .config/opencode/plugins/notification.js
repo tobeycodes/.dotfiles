51362de004950e980110c147b6603231c6e59ceb
tobey <tobey@tobey.codes>
2026-03-02 16:53:11 +0700

    feat: add opencode notification

---
+++ .config/opencode/plugins/notification.js
@@ -0,0 +1,15 @@
+export const NotificationPlugin = async ({
+  project: _project,
+  client: _client,
+  $,
+  directory: _directory,
+  worktree: _worktree,
+}) => {
+  return {
+    event: async ({ event }) => {
+      if (event.type === "session.idle") {
+        await $`osascript -e 'display notification "OpenCode Code needs your attention" with title "OpenCode" sound name "Glass"'`;
+      }
+    },
+  };
+};
