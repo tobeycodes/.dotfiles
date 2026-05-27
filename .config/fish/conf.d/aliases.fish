6817d817733130770068a84712ba0ae778797d09
tobey <tobey@tobey.codes>
2026-05-27 11:18:46 +0100

    feat: add socket firewall aliases

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,3 +3,7 @@ alias code 'nvim'
 alias oc 'opencode'
 alias wsc 'wt switch --create --no-cd'
 alias mrd 'mise run dev'
+alias npm "sfw npm"
+alias bun "sfw bun"
+alias pnpm "sfw pnpm"
+alias yarn "sfw yarn"
