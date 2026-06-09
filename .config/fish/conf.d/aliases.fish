1cc570babda467cc21c5fad192077c2ca81380a6
tobey <tobey@tobey.codes>
2026-06-10 00:54:04 +0100

    chore: remove mrd alias

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -2,7 +2,6 @@ alias c 'clear'
 alias code 'nvim'
 alias oc 'opencode'
 alias wsc 'wt switch --create --no-cd'
-alias mrd 'mise run dev'
 alias npm "sfw npm"
 alias bun "sfw bun"
 alias pnpm "sfw pnpm"
