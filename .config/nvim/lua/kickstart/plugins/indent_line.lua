c0ee95cb3be3b7b9ebd0f82abf328055ab3a4688
tobey <tobey@tobey.codes>
2026-07-08 17:44:26 +0100

    chore: update kickstart

--- .config/nvim/lua/kickstart/plugins/indent_line.lua
+++ .config/nvim/lua/kickstart/plugins/indent_line.lua
@@ -1,13 +1,6 @@
----@module 'lazy'
----@type LazySpec
-return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    ---@module 'ibl'
-    ---@type ibl.config
-    opts = {},
-  },
-}
+-- Add indentation guides even on blank lines
+
+-- Enable `lukas-reineke/indent-blankline.nvim`
+-- See `:help ibl`
+vim.pack.add { 'https://github.com/lukas-reineke/indent-blankline.nvim' }
+require('ibl').setup {}
